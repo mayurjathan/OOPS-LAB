f50e185e3a087648c5d2119504d6a4e50c0a9bac
mayurjathan <mayurjathan999@gmail.com>
2023-08-13 11:49:16 +0530

    calculator half done

---
+++ lab 2/calculator.java
@@ -0,0 +1,26 @@
+/*Create the equivalent of a four-function calculator. The program should request the user to
+enter a number, an operator, and another number. (Use floating point.) It should then carry
+out the specified arithmetic operation: adding, subtracting, multiplying, or dividing the two
+numbers. Use a switch statement to select the operation. Finally, display the result. When
+it finishes the calculation, the program should ask if the user wants to do another calculation.
+The response can be 'y' or 'n'. [Hint: use do-while loop]
+Example
+Enter first number, operator, second number: 10 / 3
+Answer = 3.333333
+Do another (y/n)? n */
+import java.util.Scanner;
+class calculator {
+    public static void main(String[] args) {
+        Scanner c = new Scanner(System.in);
+        int n1,n2;
+        char y;
+        System.out.println("Enter first number : ");
+        n1=c.nextInt();
+         System.out.println("Enter second number : ");
+         n2=c.nextInt();
+         System.out.println();
+
+        
+
+    
+}
