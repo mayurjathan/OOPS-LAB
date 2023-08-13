fc0f491fd2498f8789f03ed6308a8b2cd5ca2672
mayurjathan <mayurjathan999@gmail.com>
2023-08-13 16:42:01 +0530

    lab 2 done

---
+++ lab 2/Ad Qn/output.java
@@ -0,0 +1,18 @@
+/*Write a Java program to execute the following statements. Observe and analyze the
+outputs
+a. boolean x =true; b. boolean x =true;
+int y = x; int y =(int)x;*/
+import java.util.Scanner;
+ class output {
+    public static void main(String[] args) {
+        Scanner c = new Scanner(System.in);
+        //boolean x1=true;                
+        //int y1=x1; // This line will cause a compilation error You need to use a proper conversion
+       //System.out.println(y1);
+        //boolean x2=true;
+        //int y2=(int)x2; // This line will cause a compilation error You need to use a proper conversion
+        //System.out.println(y2);
+    }
+}
+
+
--- lab 2/Ad Qn/ternary.java
+++ lab 2/Ad Qn/ternary.java
@@ -5,19 +5,14 @@ class ternary {
     public static void main(String[] args) {
         int a,b,c,max,min; 
 
-        System.out.println("Enter 3 numbers\n");
+        System.out.println("Enter 3 numbers ");
         Scanner sc = new Scanner(System.in);
         a = sc.nextInt();
         b = sc.nextInt();
         c = sc.nextInt();
         sc.close();
-
         max = (a>b) ? ( (a>c) ? a : c) : ((b > c) ? b : c);
         min = (a<b) ? ( (a<c) ? a : c) : ((b < c) ? b : c);
-        
-
         System.out.println("max = " + max + " min = " + min);
-
-
     }
 }
